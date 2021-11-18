7115d4f5813e36e1abcfa65384ace468a750f3bf
SystemCallBeam <90573740+SystemCallBeam@users.noreply.github.com>
2021-11-19 00:38:03 +0700

    Update maze.cpp

--- maze.cpp
+++ maze.cpp
@@ -2,6 +2,26 @@
 using namespace std;
 #define n 4
 
+bool solve(int maze[n][n])
+{
+    int result[n][n] = {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    };
+
+    //Not found case
+    if (isFound(maze, 0, 0, result) == false)
+    {
+        cout << "Not found" << endl;
+        return false;
+    }
+
+    printMaze(result);
+    return true;
+}
+
 int main()
 {
     int maze[n][n] = {
