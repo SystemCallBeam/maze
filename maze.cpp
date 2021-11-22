1df83a67f156373ac0b0b1de76ecff2a8d199625
SystemCallBeam <90573740+SystemCallBeam@users.noreply.github.com>
2021-11-22 21:15:35 +0700

    Update maze.cpp

--- maze.cpp
+++ maze.cpp
@@ -22,6 +22,53 @@ bool solve(int maze[n][n])
     return true;
 }
 
+bool isFound(int maze[n][n], int x, int y, int result[n][n])
+{
+    //Goal Case
+    if (x == n - 1 && y == n - 1 && maze[x][y] == 1)
+    {
+        result[x][y] = 1;
+        return true;
+    }
+
+    //Moving case
+    if (isOnMaze(maze, x, y) == true)
+    {
+        if (result[x][y] == 1)
+        {
+            return false;
+        }
+
+        result[x][y] = 1;
+
+        //Move to +x
+        if (isFound(maze, x + 1, y, result) == true)
+        {
+            return true;
+        }
+        //Move to -x
+        if (isFound(maze, x - 1, y, result) == true)
+        {
+            return true;
+        }
+        //Move to +y
+        if (isFound(maze, x, y + 1, result) == true)
+        {
+            return true;
+        }
+        //Move to -y
+        if (isFound(maze, x, y - 1, result) == true)
+        {
+            return true;
+        }
+
+        result[x][y] = 0;
+        return false;
+    }
+
+    return false;
+}
+
 int main()
 {
     int maze[n][n] = {
